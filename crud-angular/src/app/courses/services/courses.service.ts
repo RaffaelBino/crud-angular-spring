c7bddbceb324e3debc0fb6a9d41fd7779b8fa410
Raffael Bino <raffael.bino@techlead.local>
2022-04-22 12:03:09 -0300

    Adição de spinner de carregamento e estilização dele

--- crud-angular/src/app/courses/services/courses.service.ts
+++ crud-angular/src/app/courses/services/courses.service.ts
@@ -1,8 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { first, tap } from 'rxjs/operators';
+import { delay, first, tap } from 'rxjs/operators';
 import { Course } from '../model/course';
-
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +14,7 @@ export class CoursesService {
   list() {
     return this.httpClient.get<Course[]>(this.API).pipe(
       first(),
+      delay(5000),
       tap((courses) => console.log(courses))
     );
   }
